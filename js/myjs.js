5f5651f887e8184e7f8486ba65d3cd74089b85e3
Drago-12 <133905417+Drago-12@users.noreply.github.com>
2023-05-17 23:43:29 +0530

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,20 @@
+
+
+$('#heading').click(function(){
+    $('#paraone').slideToggle();
+})
+$('#headingtwo').click(function(){
+    $('#paratwo').slideToggle();
+})
+$('#headingthree').click(function(){
+    $('#parathree').slideToggle();
+})
+$('#headingfour').click(function(){
+    $('#parafour').slideToggle();
+})
+$('#headingfive').click(function(){
+    $('#parafive').slideToggle();
+})
+$('#headingsix').click(function(){
+    $('#parasix').slideToggle();
+})
